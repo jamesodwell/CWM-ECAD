df86f382597b627a656b46adf439e3f40570606a
Cloud User <centos@ip-172-31-4-48.eu-west-2.compute.internal>
2020-06-15 15:52:28 +0000

    A partially complete Ex3

--- Ex3/top.v
+++ Ex3/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #3 
-// Student Name:
-// Date: 
+// Student Name:James Odwell	
+// Date:15/06/2020
 //
 //  Description: In this exercise, you need to design an up / down counter, where 
 //  if the rst=1, the counter should be set to zero. If enable=0, the value
@@ -20,11 +20,30 @@
 
 module counter(
     //Todo: add ports 
+	input clk, input rst, input enable, input direction,
+	output reg [7:0]counter_out
 
     );
                     
     //Todo: add registers and wires, if needed
 
     //Todo: add user logic
+	always@(posedge clk or posedge rst) begin
+	if(rst)
+	counter_out <= 0;
+	else
+	    if(direction)
+		if(enable)
+		counter_out <= counter_out+1;
+		else
+		counter_out <= counter_out;
+	    else
+		if(enable)
+		counter_out <= counter_out-1;
+		else
+		counter_out <= counter_out;
+	end
+	    	
+		
       
 endmodule
--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #3 - Up/Down counter
-// Student Name:
-// Date: 
+// Student Name:James Odwell
+// Date:15/06/2020
 //
 // Description: A testbench module to test Ex3 - counter
 // Guidance: start with simple tests of the module (how should it react to each 
@@ -14,10 +14,22 @@ module top_tb(
     );
     
 //Todo: Parameters
+	parameter CLK_PERIOD = 10;
 
 //Todo: Regitsers and wires
+	reg clk;
+	reg rst;
+	reg enable;
+	reg direction;
+	reg [7:0]counter_out
 
 //Todo: Clock generation
+	initial
+    	begin
+       	clk = 1'b0;
+       	forever
+        #(CLK_PERIOD/2) clk=~clk;
+     	end
 
 //Todo: User logic
     
