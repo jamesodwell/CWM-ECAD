ad0a760ed203e84c71e2fdbc6cad0629263f1f25
Cloud User <centos@ip-172-31-4-48.eu-west-2.compute.internal>
2020-06-16 15:54:35 +0000

    Partial solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 
-// Student Name:
-// Date: 
+// Student Name:James Odwell
+// Date:16/06/2020
 //
 //  Description: In this exercise, you need to design an electronic dice, following
 //  the diagram provided in the exercises documentation. The dice rolls as long as
@@ -15,4 +15,36 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module dice(input clk, rst, button, output reg [2:0] throw);
+
+always @ (*) begin
+	if (rst)
+	throw<=3'b000;
+	else begin
+		if (button) begin
+	        case (throw)
+	        3'b000: throw<=3'b001;
+	        3'b001: throw<=3'b010;
+	        3'b010: throw<=3'b011;
+	        3'b011: throw<=3'b100;
+	        3'b100: throw<=3'b101;
+	        3'b101: throw<=3'b110;
+	        3'b110: throw<=3'b001;
+	        3'b111: throw<=3'b001;
+	      	endcase
+	    	end
+		else begin
+			if ((throw==3'b000) || (throw==3'b111))
+			throw<=3'b001;
+			else
+			throw<=throw;
+		end
+		end 
+	end 
+
+endmodule
+
 
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,75 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Electronic Dice
-// Student Name:
-// Date: 
+// Student Name:James Odwell
+// Date:16/06/2020
 //
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+	);
+
+//Parameters
+	parameter CLK_PERIOD = 10;
+
+//Registers and wires
+	reg clk;
+	reg rst;
+	reg button;
+	wire [2:0] throw;
+	reg [2:0] throw2;
+	reg err;
+
+//Todo: Clock generation
+	initial
+    	begin
+       	clk = 1'b0;
+       	forever
+        #(CLK_PERIOD/2) clk=~clk;
+     	end
+
+	initial 
+	begin
+	rst = 1;
+	#(CLK_PERIOD) rst = 0;
+	end
+
+//Todo: User logic
+	initial 
+	begin
+	err = 0;
+	throw2 = throw;
+		forever 
+		begin
+		#CLK_PERIOD
+		
+		if (rst==1)
+			if (throw!=3'b000) begin
+	 		$display("***Test Failed*** didn't reset properly");
+           		err=1;
+			end
+		else 	
+		
+
+		
+	end
+//Finish test, check for success
+    initial begin
+	#200
+	if (err==0)
+	$display("***TEST PASSED!***");
+	$finish;
+    end
+
+//Instantiate counter module
+dice top (
+  .clk (clk),
+  .rst (rst),
+  .button (button),
+  .throw (throw)
+  );
+
+endmodule
