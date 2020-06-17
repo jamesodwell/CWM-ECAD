fdeb55f200c729b5c40e26519305426d0a0fac41
Cloud User <centos@ip-172-31-4-48.eu-west-2.compute.internal>
2020-06-17 15:22:06 +0000

    A complete solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name:James Odwell
+// Date:17/06/2020
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -13,3 +13,65 @@
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module traffic(
+	input clk,
+	output reg red,
+	output reg amber,
+	output reg green
+  );
+
+  //add registers and wires
+	reg [2:0] current;
+
+  //add user logic
+
+	always @ (posedge clk)
+	  begin
+	  current[0]<=red;
+	  current[1]<=amber;
+	  current[2]<=green;
+
+	  if (current==3'b001) 
+	  begin
+	    red<=1;
+	    amber<=1;
+	    green<=0;
+	  end
+	else begin
+	  if (current==3'b011)
+	  begin
+	    red<=0;
+	    amber<=0;
+	    green<=1;
+	  end
+
+	    else begin
+	      if (current==3'b100)
+	       begin
+	       red<=0;
+	       amber<=1;
+	       green<=0;
+	       end
+
+	      else begin
+	 	if (current==3'b010)
+	 	begin
+		 red<=1;
+	   	 amber<=0;
+		 green<=0;
+	  	end
+
+		else begin
+		 red<=1;
+		 amber<=0;
+	  	 green<=0;
+	  	end
+	      end 
+	    end
+	 end 
+
+	end 
+
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,78 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Traffic Lights
-// Student Name:
-// Date: 
+// Student Name:James Odwell
+// Date:17/06/2020
 //
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+	);
+
+//Parameters
+	parameter CLK_PERIOD = 10;
+
+//Registers and wires
+	reg clk;
+	wire red;
+	wire amber;
+	wire green;
+	reg [2:0] current;
+	reg [2:0] previous;
+	reg err;
+
+//Generate clock
+	initial
+	 begin
+	  clk = 1'b1;
+	  forever
+	   #(CLK_PERIOD/2) clk = ~clk;
+	 end
+
+//user logic
+
+	initial begin
+	  err=0;
+	  previous = current;
+	  #1
+	  forever begin
+	    #(2*CLK_PERIOD)
+
+	  if ((current==3'b001||current==3'b011||current==3'b100||current==3'b010)==0) begin
+	      $display("***Test Failed***");
+	      err=1;
+	    end
+
+	    else begin
+	      if ( ((previous==3'b001) && (current!=3'b011)) || ((previous==3'b011) && (current!=3'b100)) || ((previous==3'b100) && (current!=3'b010)) || ((previous==3'b010) &&(current!=3'b001)) )
+	      begin
+		$display("***Test Failed*** Incorrect sequence");
+		err=1;
+	      end
+	    end 
+
+	  previous = current;
+	  current[0] = red;
+	  current[1] = amber;
+	  current[2] = green;
+	  end 
+
+	end 
+
+    initial begin
+	#200
+	if (err==0)
+	 $display("***TEST PASSED!***");
+	$finish;
+    end
+
+traffic top (
+  .clk (clk),
+  .red (red),
+  .amber (amber),
+  .green (green)
+  );
+endmodule
