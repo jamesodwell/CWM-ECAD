c6fa12c3d139981556fb320d9c341fd03299d5d6
Cloud User <centos@ip-172-31-4-48.eu-west-2.compute.internal>
2020-06-19 15:57:22 +0000

    A complete solution to Ex7

--- Ex7/top.v
+++ Ex7/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #7
-// Student Name:
-// Date: 
+// Student Name:James Odwell
+// Date:19/06/2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory.
@@ -13,4 +13,21 @@
 //           result[4:0]
 //////////////////////////////////////////////////////////////////////////////////
 
+module tt(
+	input clk,
+	input read,
+	input [2:0]a,
+	input [2:0]b,
+	output [5:0]result
+	);
 
+blk_mem_gen_0 tt_mem (
+	.clka(clk),
+	.ena(read),
+	.wea(1'b0),
+	.addra({a,b}),
+	.dina(6'h0),
+	.douta(result)
+	);
+
+endmodule
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -1,9 +1,43 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #7 - Times Table
-// Student Name:
-// Date: 
+// Student Name: James Odwell
+// Date: 19/06/2020
 //
 // Description: A testbench module to test Ex7 - Times Table
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/100ps
+
+module top_tb(
+);
+
+parameter clk_period = 10;
+reg clk, read; 
+reg [2:0] a; 
+reg [2:0] b;
+wire [5:0] result;
+
+initial begin
+	clk = 1'b0;
+	forever
+	#(clk_period/2)clk = ~clk; 		
+	end
+
+initial begin
+	read = 0;	 
+	a = 0; 
+	b = 0;					
+	#(10*clk_period)
+	read = 1;
+	forever begin
+		#(2*clk_period) 				
+		$display("a=%d b=%d result=%d", a, b, result);
+		a=a+1;
+		b=(a==0)?b+1:b;
+		end
+		end
+
+tt tt(.clk(clk), .a(a), .b(b), .read(read), .result(result)); 
+
+endmodule
